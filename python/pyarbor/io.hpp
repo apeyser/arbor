ab9521bf7ab883de1061fe94368a964798009005
Alexander Peyser <a.peyser@fz-juelich.de>
2017-12-16 17:51:02 +0100

    Cleanup up draft

--- python/pyarbor/io.hpp
+++ python/pyarbor/io.hpp
@@ -16,19 +16,11 @@
 namespace arb {
 namespace io {
 
-class usage_error: public std::runtime_error {
-public:
-    template <typename S>
-    usage_error(S&& whatmsg): std::runtime_error(std::forward<S>(whatmsg)) {}
-};
-
 /// Helper function for loading a vector of spike times from file
 /// Spike times are expected to be in milli seconds floating points
 /// On spike-time per line
 
 std::vector<time_type>  get_parsed_spike_times_from_path(arb::util::path path);
-
-std::ostream& operator<<(std::ostream& o, const options& opt);
 
 } // namespace io
 } // namespace arb
